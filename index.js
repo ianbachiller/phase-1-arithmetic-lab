596963c460ade5632353391e17addc1a9b794529
Ian Bachiller <ianbachise@gmail.com>
2024-01-31 10:51:54 -0500

    complete phase 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 85;
+let num4 = 9;
+let mod = num3%num4;
+
+let max = Math.max(1,5,20);
